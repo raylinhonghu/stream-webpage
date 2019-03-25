3eea955a222adacc73a1e3f7ede4490cac46ea72
john <raylinhonghu@gmail.com>
2019-03-25 21:07:20 +0800

    2019/3/25

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Modal = props => {
-    return ReactDOM.createPortal(
-        <div className="ui modal dimmer visible active">
-            <div className="ui modal standard visible active">
-                i am modal
+import history from '../history';
+const modalRoot = document.querySelector('#modal');
+
+
+// <div className="ui dimmer modals visible active">
+//     <div className="ui standard modal visible active">
+//         i am modal
+//     </div>
+// </div>
+
+class Modal extends React.Component {
+
+    render() {
+        return ReactDOM.createPortal(
+            <div onClick={this.props.cancelDireact} className="ui dimmer modals visible active">
+                <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
+                    <div className="header">
+                        {this.props.title}
+                    </div>
+                    <div className="content">
+                        {this.props.description}
+                    </div>
+                    <div className="actions">
+                        {this.props.actions()}
+                    </div>
+                </div>
             </div>
-        </div>,
-        document.querySelector('#modal')
-    )
+            ,
+            modalRoot
+        )
+    }
 }
+
+export default Modal;
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import Modal from '../Modal';
 
-const StreamDelete = () => {
-    return (
-        <div>
-            del
-        </div>
-    )
+import history from '../../history';
+
+class StreamDelete extends React.Component {
+
+
+    actions = () => {
+        return (
+            <React.Fragment>
+                <div className="ui red button">
+                    DELETE
+                </div>
+                <div className="ui black button">
+                    CANCLE
+                </div>
+            </React.Fragment>
+        )
+    }
+
+    cancelDirect = () => {
+        console.log(111111111111111);
+        
+        history.push('/');
+    }
+
+    render() {
+        return (
+            <Modal
+                title="DELETE STREAM"
+                description="Are you sure you want to delete this stream?"
+                actions={this.actions}
+                cancelDirect={()=> history.push('/')}
+            />
+        )
+    }
 }
 
 export default StreamDelete;
